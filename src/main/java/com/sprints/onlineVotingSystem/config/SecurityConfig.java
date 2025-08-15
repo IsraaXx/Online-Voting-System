f27b2cdb94f3a2479ddb3ca841f732a8e6906f6f
IsraaMohamed <saramohamed2315@gmail.com>
2025-08-15 16:22:43 +0300

    created basic Spring Security config

---
+++ src/main/java/com/sprints/onlineVotingSystem/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.sprints.onlineVotingSystem.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/voter/**").hasRole("VOTER")
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(httpBasic -> {});
+
+        return http.build();
+    }
+
+}
