aceb2daf7180f38e11f98ee756e8544cbd34c3d6
IsraaMohamed <saramohamed2315@gmail.com>
2025-08-15 14:45:47 +0300

    add @ComponentScan configuration with exclude filters

--- src/main/java/com/sprints/onlineVotingSystem/OnlineVotingSystemApplication.java
+++ src/main/java/com/sprints/onlineVotingSystem/OnlineVotingSystemApplication.java
@@ -2,8 +2,18 @@ package com.sprints.onlineVotingSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
+
+@ComponentScan(
+		basePackages = "com.sprints.onlineVotingSystem",
+		excludeFilters = @ComponentScan.Filter(
+				type = FilterType.REGEX,
+				pattern = "com\\.sprints\\.onlineVotingSystem\\.legacy\\..*"
+		)
+)
 public class OnlineVotingSystemApplication {
 
 	public static void main(String[] args) {
